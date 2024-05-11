68c8a2eb4eaa8f6f97a9c9c8722d3b1bd16f01cc
Kkhokho <khoi.mm210492@gmail.com>
2024-05-11 11:32:22 +0700

    nothing

--- back-end/authentication/src/controller/auth.controller.ts
+++ back-end/authentication/src/controller/auth.controller.ts
@@ -1,7 +1,28 @@
+import { Request, Response } from "express";
+import { config } from "dotenv";
+import { BadRequest, Unauthorized } from "../util/response/error";
+
+interface ISignInReq {
+  email: string;
+  id: string;
+}
 class AuthController {
-  sign_up() {
+  async sign_up(req: Request<any, any, ISignInReq>, res: Response) {
     try {
-    } catch (error) {}
+      const api_key = req.headers["x-api-key"];
+      const { email, id } = req.body;
+      const apiKey = process.env.API_KEY || 'api-key';
+      if (!api_key) {
+        throw new BadRequest("missing api key");
+      }
+
+      if (apiKey !== api_key) {
+        throw new Unauthorized("Not Permittion");
+      }
+      
+    } catch (error) {
+
+    }
   }
 }
 
