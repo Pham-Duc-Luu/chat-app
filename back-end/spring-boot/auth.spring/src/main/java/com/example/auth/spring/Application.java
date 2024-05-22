012a5cea8a91cd039f848d7a8bb4091a92c941b7
Pham-Duc-Luu <phamducluu2003@gmail.com>
2024-05-22 15:24:24 +0700

    my-app/add mobile set up

--- back-end/spring-boot/auth.spring/src/main/java/com/example/auth/spring/Application.java
+++ back-end/spring-boot/auth.spring/src/main/java/com/example/auth/spring/Application.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(DemoApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 }
---
+++ my-app/web/app/layout.tsx
@@ -0,0 +1,43 @@
+import "./globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
+import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export default function RootLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <GoogleOAuthProvider
+            clientId={process.env.NEXT_PUBLIC_CLIENT_ID as string}
+          >
+            {children}
+          </GoogleOAuthProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
